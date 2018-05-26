ae3ad8f43a99a5d5242944084daa2628c0da532f
Chenghong Lin <690060189@qq.com>
2018-05-26 16:37:33 +0800

    412 solution

---
+++ 412-Fizz Buzz/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> ans;
+        for(int i=1; i<=n; i++){
+            if(i%15==0){
+                ans.push_back("FizzBuzz");
+            }else if(i%5==0){
+                ans.push_back("Buzz");
+            }else if(i%3==0){
+                ans.push_back("Fizz");
+            }else{
+                string str = to_string(i);
+                ans.push_back(str);
+            }
+        }
+        return ans;
+    }
+};
